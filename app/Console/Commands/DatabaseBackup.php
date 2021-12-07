e61a86362025e43ee93d2013216e37f659e2a60f
Sachin Dev <dev.sachin30@gmail.com>
2021-12-08 00:22:28 +0530

    backup with console command

---
+++ app/Console/Commands/DatabaseBackup.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class DatabaseBackup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'database:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Takes a backup of db daily';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Artisan::call('backup:run');
+    }
+}
